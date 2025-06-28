380a5c4f8a4e443a0e437535973a369ef2d0f618
Aayush-Sahay <aayushsahay1062@gmail.com>
2025-06-28 18:47:50 +0530

    Adding Programs Taught in class (28-06-2025)

---
+++ day1_ClassTasks/program1.java
@@ -0,0 +1,13 @@
+/*
+* Program on date : 28th June 2025
+* Purpose : To demonstrate the print & println function
+* */
+
+
+public class program1 {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print("Bye");
+        System.out.print("\nHello"+" "+"BYE");
+    }
+}
---
+++ day1_ClassTasks/program2.java
@@ -0,0 +1,15 @@
+/*
+* Program 2 : To show the use of the concatenation operator
+* Date: 28 june 2025
+* */
+
+
+
+public class program2 {
+    public static void main(String[] args) {
+        int x=10,y=20;
+        System.out.println("No.="+x);
+        System.out.println("No.="+x+y);
+        System.out.println("No.="+(x+y));
+    }
+}
