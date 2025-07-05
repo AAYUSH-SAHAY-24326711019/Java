1fc6a58d523f9a23c0efa6cf52ff5dd70d20a566
Aayush-Sahay <aayushsahay1062@gmail.com>
2025-07-05 08:13:26 +0530

    Adding Codes 04-07-2025 Exception Handling

---
+++ Day5/program2.java
@@ -0,0 +1,22 @@
+/*
+* Date : 04-07-2025
+* Purpose : To use the string for the handling of the
+* Null pointer Exception.
+* */
+
+public class program2 {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println("the string contains :"+str);
+        try{
+            System.out.println(str.toUpperCase());
+        }
+        catch (Exception e){
+            System.out.println("Null string cannot be upper case");
+        }
+    }
+
+    //output
+    //the string contains :null
+    //Null string cannot be upper case
+}
---
+++ Day5/program4.java
@@ -0,0 +1,26 @@
+/*
+* Date : 4-7-2025
+* Purpose:Try-catch and finally block example
+*
+* */
+
+public class program4 {
+    //exception code from program 3
+    public static void main(String[] args) {
+        String input = "Hello";
+        System.out.println("String contains :"+input);
+        try{
+            System.out.println(Integer.parseInt(input));
+        } catch (Exception e) {
+            System.out.println("String to Int conv. exception"+e.getMessage());
+        }
+        finally{
+            System.out.println("Success after exception-handled");
+        }
+    }
+    }
+
+    //output
+//String contains :Hello
+//String to Int conv. exceptionFor input string: "Hello"
+//Success after exception-handled
