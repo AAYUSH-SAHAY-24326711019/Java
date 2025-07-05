4bd84c4fa4304464a2a4a1f7c62d6cfeb06e58d9
Aayush-Sahay <aayushsahay1062@gmail.com>
2025-07-05 08:39:05 +0530

    Uploaded 04-07-2025 Exception Handling Codes

---
+++ day5/program1.java
@@ -0,0 +1,21 @@
+/*** Date: 4-7-2025
+* Programs on exception handling in java
+* This program shows the happening of exceptions
+*/
+import java.util.Scanner;
+public class program1 {
+    public static void main(String[] args) {
+        Scanner z = new Scanner(System.in);
+        int divisor = 0;
+        System.out.print("User, enter a number->");
+        int noInp = z.nextInt();
+        try {
+            int out = noInp / divisor;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+//output
+//User, enter a number->10
+//divide  by zero exception
---
+++ day5/program7.java
@@ -0,0 +1,21 @@
+/** Date 4-7-2025
+* Program on the nested Catch Block
+*/
+public class program7 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(10 / 0);
+        } catch (Exception e) {
+            try {
+                String aa = null;
+                System.out.println(aa.toLowerCase());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        System.out.println("Finally the program ends");
+    }
+}
+//output
+//Cannot invoke "String.toLowerCase()" because "aa" is null
+//Finally the program ends
